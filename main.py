de42b238f8f9e6baf86079a530c9adf8afb634cf
Zham_Raf <raf280400@mail.ru>
2025-08-22 18:26:38 +0300

    version2

---
+++ main.py
@@ -0,0 +1,16 @@
+#скачать homebrew 
+# запустить его в терминале
+# cкачать гит через терминал и хоум брю и задать имя через команду /
+# git config --global user.name ""
+# git config --global user.email ""
+# создать папку и запустить гит
+
+
+
+print("hello world")
+# git init -запуститт гит на компе  213
+#git add . - сохранить все
+# git status -проверить статус wwe 
+i = 7 
+if i in range(10):
+    print("Я люблю програмировать")
